db2b60d8a9c75e5a6a2d8ac3dc157bd3fdc3cc6d
markchen8717 <29904105+markchen8717@users.noreply.github.com>
2019-11-18 00:05:42 -0500

    Implemented boilerpate expo camera

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import IngredientsPage from './Screens/IngredientsPage'
-import CameraPage from './Screens/CameraPage'
+import IngredientsPage from './Screens/IngredientsPage';
 
+import CameraPage from './Screens/CameraPage';
 
 var ingrdnts_to_description = [
   { 'Soy Sauce': 'A Chinese sauce' },
@@ -12,12 +12,11 @@ var ingrdnts_to_description = [
 export default function App() {
   const [ingrdnts, setIngrdnts] = useState(ingrdnts_to_description);
   let content = <CameraPage/>;
-  if (true){
+  //Once picture is taken render ingredients page
+  if (false){
     content = <IngredientsPage ingrdnts = {ingrdnts}/>;
   }
   return (
-    <View>
-      {content}
-    </View>
+      content
   );
 }
--- Screens/CameraPage.js
+++ Screens/CameraPage.js
@@ -1,16 +1,55 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
-const CameraPage = (props) =>
-{
-    return (
-        <View>
-            <Text>
-                Camera Page
-            </Text>
-        </View>
-    );
-}
+export default class CameraPage extends React.Component {
+  camera = null;
+
+  state = {
+      hasCameraPermission: null,
+  };
+
+  async componentDidMount() {
+      const camera = await Permissions.askAsync(Permissions.CAMERA);
+      const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+      const hasCameraPermission = (camera.status === 'granted' && audio.status === 'granted');
+
+      this.setState({ hasCameraPermission });
+  };
+
+  render() {
+      const { hasCameraPermission } = this.state;
+
+      if (hasCameraPermission === null) {
+          return <View />;
+      } else if (hasCameraPermission === false) {
+          return <Text>Access to camera has been denied.</Text>;
+      }
+
+      return (
+          <View>
+              <Camera
+                  style = {styles.preview}
+                  ref={camera => this.camera = camera}
+              />
+          </View>
+      );
+  };
+};
 
 
-export default CameraPage
+const { width: winWidth, height: winHeight } = Dimensions.get('window');
+
+const styles =  StyleSheet.create({
+    preview: {
+        height: winHeight,
+        width: winWidth,
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
